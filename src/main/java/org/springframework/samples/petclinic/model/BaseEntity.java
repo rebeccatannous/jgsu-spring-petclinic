9e64c1baac3cb46a67cc4c35f6ce6e132c4e6851
rtannous <rtannous@murex.com>
2022-08-05 16:13:27 +0300

    Compile maven

--- src/main/java/org/springframework/samples/petclinic/model/BaseEntity.java
+++ src/main/java/org/springframework/samples/petclinic/model/BaseEntity.java
@@ -31,7 +31,7 @@ import javax.persistence.MappedSuperclass;
  */
 @MappedSuperclass
 public class BaseEntity implements Serializable {
-
+//1
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
